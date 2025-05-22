f850585d44a53d781ebe0246cdc75517a50057fc
thangtdhe160619 <ams.w5112001@gmail.com>
2025-05-23 02:16:04 +0700

    Enemy/pathway no.1
    Add 2 enemies, pathway. Enemy don't have health or can get attacked yet.

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager main;
+
+    public Transform startPoint;
+
+    public Transform[] path;
+
+    public void Awake(){
+        main = this;
+    }
+}
